3aec5991096004511ce3a22c5ec3fb1e0279818c
PrajwalaTM <prajwala.tm@gmail.com>
2020-04-19 18:18:08 +0530

    1. Renamed export-mp3 2. Moved properties to config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+#For Spotify auth
+USERNAME = 'sm1yrttav2c5kbzxrk3jffyqo'
+CLIENT_SECRET = '0d564cc9d4b649e2bbd8231b44d40e13'
+CLIENT_ID = '358ae441a02d4a9d953cf82848cc9391'
+
+#Destination Playlist ID
+PLAYLIST_ID = '4aDCd1Kzd9mjJzReIIXEwH'
+
+#Source directory path containing .mp3
+SOURCE_PATH = '/home/prajwalatm/mp3-converter/music'
--- convert-mp3.py
+++ convert-mp3.py
@@ -4,11 +4,7 @@ import spotipy.util as util
 import os
 import urllib
 import json
-
-USERNAME = 'sm1yrttav2c5kbzxrk3jffyqo'
-CLIENT_SECRET = '0d564cc9d4b649e2bbd8231b44d40e13'
-CLIENT_ID = '358ae441a02d4a9d953cf82848cc9391'
-PLAYLIST_ID = '4aDCd1Kzd9mjJzReIIXEwH'
+from config import *
 
 def getEyeD3Tag(path):
     """"""
@@ -41,9 +37,8 @@ def getTracksAndAddToPlaylist(token,tags):
 
 
 def main():
-    dir_path = '/home/prajwalatm/mp3-converter/music'
     mp3_files = []
-    for i in os.scandir(dir_path):
+    for i in os.scandir(SOURCE_PATH):
         if i.is_file():
             mp3_files.append(i.path)
     
